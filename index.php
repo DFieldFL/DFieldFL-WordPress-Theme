2815be3fbd9ae416dda53fbb1d8fdd41f29cf60a
Brandon T. Groves <brandon.groves@gmail.com>
2013-05-01 19:45:47 -0400

    added ignore for IDEs and fix initial post loop

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
             <div class="container">
                 <div class="row">
                     <div class="span12">
-                    <?php while(have_posts()): the_posts(); ?>
-                        <?php the_content(); ?>
-                    <?php endwhile; ?>
+                    <?php if(have_posts()): ?>
+                        <?php while(have_posts()): the_post(); ?>
+                            <?php the_content(); ?>
+                        <?php endwhile; ?>
+                    <?php else: ?>
+                        Sorry, there are not posts...
+                    <?php endif; ?>
                     </div>
                 </div>
             </div>
